54903346abc43582c6cf565ac3e2071dc5d0e3f8
bugymarek <bugymarek@gmail.com>
2018-06-26 16:35:40 +0200

    added simple security API and all requirement libs.

---
+++ src/API/Aplication.java
@@ -0,0 +1,20 @@
+package API;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("webapi")
+public class Aplication extends Application {
+
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> s = new HashSet<Class<?>>();
+
+        // Annotated @Path endpoint
+        s.add(SecuredResource.class);
+        s.add(filters.SecurityFilter.class);
+
+        return s;
+    }
+}
